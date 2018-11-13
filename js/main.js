bb06f172e94856a4c81d1658a9de232a500865cd
Ekta Chavan <ekta@gethappie.me>
2018-11-13 03:27:21 -0500

    clean up

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $('#nav').affix({
 	var index=0;
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
 		if(top<-300){
 			if(index==0){	
 			
@@ -45,7 +44,6 @@ $('#nav').affix({
 			index++;
 		}
 	})
-	//console.log(nagativeValue)
 	});
 
     particlesJS("particles-js", 
@@ -171,8 +169,6 @@ $('#nav').affix({
         requestAnimationFrame(update); 
     }; 
     requestAnimationFrame(update);;
-
-    
 
   	// Portfolio isotope filter
     $(window).load(function() {
